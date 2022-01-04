c587c12ca80190184dc640bcd50f9db63238c4b2
Van Tran <tavan.it@gmail.com>
2022-01-04 10:55:11 +0100

    updated

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
 var myVar;
 
 $('#start').click(function(e){
-    var getSec = $('input#inputSec').val() || 0;  
-    var getMin = $('input#inputMin').val() || 0;
-    var getHr = $('input#inputHr').val() || 0;
-    console.log($('#inputSec').val());
+    var getSec = $('#inputSec').val() || 0;  
+    var getMin = $('#inputMin').val() || 0;
+    var getHr = $('#inputHr').val() || 0;
+    // console.log($('#inputSec').val());
     $('.sec').html(getSec);
     $('.min').html(getMin);
     $('.hr').html(getHr);
@@ -18,13 +18,17 @@ $('#reset').click(function(e){
     $('.hr').html(00);
     $('.min').html(00);
     $('.sec').html(00);
-    $('input#inputSec').val(00);
-    $('input#inputMin').val(00);
-    $('input#inputHr').val(00);
+    $('#inputSec').val(00);
+    $('#inputMin').val(00);
+    $('#inputHr').val(00);
 });
 
 function calcTime(){
+
+    myVar = setInterval(update, 1000); // every one second
+
     function update(){
+        
         $('.sec').each(function(){
             var count = parseInt($(this).html());
             if(count !== 0){
@@ -34,7 +38,7 @@ function calcTime(){
             }
         });
     };
-    myVar = setInterval(update, 1000); // every one second
+    
     function dec_min(){
         var min = parseInt($('.min').html());
             if(min !== 0){
@@ -45,17 +49,17 @@ function calcTime(){
             }
         };
     
-        function dec_hr(){
-            var hr = parseInt($('.hr').html());
-            if(hr !== 0){
-                $('.hr').html(hr-1);
-                $('.min').html(59);
-                $('.sec').html(59);
-            }else {
-                $("#audio")[0].play();
-                clearInterval(myVar); // stop setInterval
-            }
+    function dec_hr(){
+        var hr = parseInt($('.hr').html());
+        if(hr !== 0){
+            $('.hr').html(hr-1);
+            $('.min').html(59);
+            $('.sec').html(59);
+        }else {
+            $("#audio")[0].play();
+            clearInterval(myVar); // stop setInterval
         }
+    }
 };
 
 })
